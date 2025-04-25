9d438d89f1968e6042d77744262a64239bb36aa1
Darryll Lance C. Montenegro <lancem28.dlm@gmail.com>
2025-04-25 13:03:16 +0800

    Prisma Client

--- backend/prisma/generated/prisma/client/edge.js
+++ backend/prisma/generated/prisma/client/edge.js
@@ -222,11 +222,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:k4hTEW5K76qcv2o4@db.ggijcplcdcwczkuvadrx.supabase.co:5432/postgres"
+        "value": null
       }
     }
   },
