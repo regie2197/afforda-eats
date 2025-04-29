efa637422bbce3162f179b0a6b28787f622a4cc0
Lfarparan <aguinaldo.farparan@gmail.com>
2025-04-29 10:43:33 +0800

    test: api_registration - Added negative path api tests

---
+++ cypress/e2e/api-testing/login-api-testing.cy.js
@@ -0,0 +1,54 @@
+import { createUser, generateEmail, generateUsername } from '../../support/user-faker.utils.js'
+
+describe('API - REGISTRATION testing', () => {
+    const newUser = createUser()
+
+    before(() => {
+        cy.api({
+            method: 'POST',
+            url: "http://localhost:4000/api/register",
+            body: newUser
+        })
+    })
+
+    it('POST request for Login In using email', () => {
+        cy.api({
+            method: 'POST',
+            url: "http://localhost:4000/api/login",
+            body: {
+                email: newUser.email,
+                password: newUser.password
+            }
+        }).should((response) => {
+            expect(response.status).to.eq(200)
+        })
+    })
+
+    it('POST request for Login In using username', () => {
+        cy.api({
+            method: 'POST',
+            url: "http://localhost:4000/api/login",
+            body: {
+                username: newUser.username,
+                password: newUser.password
+            }
+        }).should((response) => {
+            expect(response.status).to.eq(200)
+        })
+    })
+
+
+    // it('DELETE request for deleting user by ID', () => {
+    //     const credential = `${newUser.username}:${newUser.password}`
+    //     cy.api({
+    //         method: 'DELETE',
+    //         url: "http://localhost:4000/api/user/" + userId,
+    //         headers: {
+    //             Authorization: 'Basic ' + btoa(credential),
+    //         }
+    //     }).should((response) => {
+    //         expect(response.status).to.eq(204)
+    //     })
+    // })
+
+})
---
+++ cypress/e2e/api-testing/register-api-testing.cy.js
@@ -0,0 +1,125 @@
+import { createUser} from '../../support/user-faker.utils.js'
+
+describe('API - REGISTRATION testing', () => {
+    const newUser = createUser()
+
+    it('Verify successful POST request for creating a new user', () => {
+        cy.api({
+            method: 'POST',
+            url: "http://localhost:4000/api/register",
+            body: newUser
+        }).should((response) => {
+            expect(response.status).to.eq(201)
+        })
+    })
+
+    it('Verify unsuccessful user registration when there are missing required field in the request body', () => {
+        const reg_missingField = createUser()
+        cy.api({
+            method: 'POST',
+            url: "http://localhost:4000/api/register",
+            body: { 
+                username: reg_missingField.username,
+                password: reg_missingField.password,
+                email: reg_missingField.email
+            },
+            failOnStatusCode: false
+        }).should((response) => {
+            expect(response.status).to.eq(400)
+            expect(response.body.error).to.eq("All fields are required.")
+        })
+    })
+
+
+    it('Verify unsuccessful user registration when the required field values contains leading and trailing spaces', () => {
+        const reg_spaceAround = createUser()
+        cy.api({
+            method: 'POST',
+            url: "http://localhost:4000/api/register",
+            body: { ...reg_spaceAround, firstName: "Sofia  ", lastName: "  Lawson" },
+            failOnStatusCode: false
+        }).should((response) => {
+            expect(response.status).to.eq(400)
+            expect(response.body.error).to.eq("Fields must not have leading or trailing spaces.")
+        })
+    })
+
+    it('Verify unsuccessful user registration when email field value contains whitespace', () => {
+        const reg_emailWhitespace = createUser()
+        cy.api({
+            method: 'POST',
+            url: "http://localhost:4000/api/register",
+            body: { ...reg_emailWhitespace, email: "sofia @email.com"},
+            failOnStatusCode: false
+        }).should((response) => {
+            expect(response.status).to.eq(400)
+            expect(response.body.error).to.eq("Email must not contain spaces.")
+        })
+    })
+
+    it('Verify unsuccessful user registration when username field value contains whitespace', () => {
+        const reg_usernameWhitespace = createUser()
+        cy.api({
+            method: 'POST',
+            url: "http://localhost:4000/api/register",
+            body: { ...reg_usernameWhitespace, username: "John Carl"},
+            failOnStatusCode: false
+        }).should((response) => {
+            expect(response.status).to.eq(400)
+            expect(response.body.error).to.eq("Username must not contain spaces.")
+        })
+    })
+
+    it('Verify unsuccessful user registration when email is on an invalid format', () => {
+        const reg_invalidEmail = createUser()
+        cy.api({
+            method: 'POST',
+            url: "http://localhost:4000/api/register",
+            body: { ...reg_invalidEmail, email: "sofiaemail@com"},
+            failOnStatusCode: false
+        }).should((response) => {
+            expect(response.status).to.eq(400)
+            expect(response.body.error).to.eq("Invalid email format.")
+        })
+    })
+
+    it('Verify unsuccessful user registration when the password field value has less than 6 character', () => {
+        const reg_invalidPass = createUser()
+        cy.api({
+            method: 'POST',
+            url: "http://localhost:4000/api/register",
+            body: { ...reg_invalidPass, password: "l0co?"},
+            failOnStatusCode: false
+        }).should((response) => {
+            expect(response.status).to.eq(400)
+            expect(response.body.error).to.eq('Password must be at least 6 characters long.')
+        })
+    })
+
+    it('Verify unsuccessful user registration when the entered email already exists in the database', () => {
+        const reg_sameEmail = createUser()
+        cy.api({
+            method: 'POST',
+            url: "http://localhost:4000/api/register",
+            body: { ...reg_sameEmail, email: newUser.email },
+            failOnStatusCode: false
+        }).should((response) => {
+            expect(response.status).to.eq(400)
+            expect(response.body.error).to.eq('Email is already taken.')
+        })
+    })
+
+    it('Verify unsuccessful user registration when the entered username already exists in the database', () => {
+        const reg_sameUsername = createUser()
+        cy.api({
+            method: 'POST',
+            url: "http://localhost:4000/api/register",
+            body: { ...reg_sameUsername, username: newUser.username},
+            failOnStatusCode: false
+        }).should((response) => {
+            expect(response.status).to.eq(400)
+            expect(response.body.error).to.eq('Username is already taken.')
+        })
+    })
+
+})
