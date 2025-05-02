bc250215fd8caf797ce25e89541b63985cc6416c
gdimayuga <GDIMAYUGA@VERTERE-GS.COM>
2025-05-02 08:09:43 +0800

    review api test

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+export default {
+  e2e: {
+    setupNodeEvents(on, config) {
+      supportFile: 'cypress/support/e2e.js'
+      // implement node event listeners here
+    },
+  },
+};
---
+++ cypress/support/reviewdata.js
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker';
+ 
+export function reviewData() {
+    return {
+        "content": "good",
+        "rating": 3,
+        "userId": 15,
+        "foodId": 4,
+        "storeId": 5
+    };
+}
