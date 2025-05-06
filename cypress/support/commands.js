592dbdac7ed51c0835c1337d5e6dcaf59eabe2c1
Allan Magtibay Jr. <amagtibay@vertere-gs.com>
2025-05-06 13:12:15 +0800

    Fix: Merge conflict in commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -75,46 +75,6 @@ Cypress.Commands.add('AffordaEatsGenerateStoreFile', (NewStore) => {
     cy.writeFile('cypress/fixtures/AffordaEatsStoreInfo.json', NewStore)
 })
 
-<<<<<<< HEAD
-
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
-=======
 Cypress.Commands.add('AffordEatsGenerateUpdateFoodFile', (UpdateFile) => {
     cy.writeFile('cypress/fixtures/AffordaEatsUpdatefood.json', UpdateFile)
 } )
->>>>>>> abe755679f6d4fbeab146c52815edfad5b94abf1
